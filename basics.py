db20e1ee4d745de20d3ba7e4ccafe3c229d5c514
khanwald21 <oscarmutamba77@gmail.com>
2023-03-27 07:25:59 +0300

    added math libraries

--- basics.py
+++ basics.py
@@ -1,1 +1,11 @@
+a=5
+b=20
+k=int(a+b)
+print(k)
+z=int(input('enter a number: '))
+y=float(input('enter a second number: '))
+temperature=float(z+y)
+#print('the teperature is: '+ str (temperature))
+print(f'the temperature is: {temperature}')
+
 
